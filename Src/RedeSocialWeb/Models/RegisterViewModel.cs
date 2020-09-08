ff9b56d41ecd53dafe974f31afe28a747d797a49
Duarte <guilhermeduartt21@gmail.com>
2020-09-08 19:57:30 -0300

    feat: rede social

---
+++ Src/RedeSocialWeb/Models/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedeSocialWeb.Models
+{
+    public class RegisterViewModel
+    {
+        public String Name { get; set; }
+        public string UserName { get; set; }
+        public String Email { get; set; }
+        public DateTime DtBirthday { get; set; }
+        public String Password { get; set; }
+        public String ConfirmPassword { get; set; }
+    }
+}
